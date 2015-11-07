2dbde009b11ea52f06b5f7fcc11b006cb589db8d
Denevy <Denevy>
2015-11-06 22:43:39 -0600

    Agregando iconos

--- site/src/Umg/VotacionBundle/Controller/DefaultController.php
+++ site/src/Umg/VotacionBundle/Controller/DefaultController.php
@@ -146,25 +146,19 @@ class DefaultController extends Controller
 
     public function chartAction()
     {
-        $ob = new Highchart();
-        $ob->chart->renderTo('piechart');
-        $ob->title->text('Browser market shares at a specific website in 2010');
-        $ob->plotOptions->pie(array(
-            'allowPointSelect'  => true,
-            'cursor'    => 'pointer',
-            'dataLabels'    => array('enabled' => false),
-            'showInLegend'  => true
-        ));
-        $data = array(
-            array('Firefox', 45.0),
-            array('IE', 26.8),
-            array('Chrome', 12.8),
-            array('Safari', 8.5),
-            array('Opera', 6.2),
-            array('Others', 0.7),
-        );
-        $ob->series(array(array('type' => 'pie','name' => 'Browser share', 'data' => $data))); 
-        return $this->render('UmgVotacionBundle:Default:chart.html.twig', array(
+    // Chart
+    $series = array(
+        array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
+    );
+
+    $ob = new Highchart();
+    $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
+    $ob->title->text('Chart Title');
+    $ob->xAxis->title(array('text'  => "Horizontal axis title"));
+    $ob->yAxis->title(array('text'  => "Vertical axis title"));
+    $ob->series($series);
+
+    return $this->render('UmgVotacionBundle:Default:chart.html.twig', array(
             'chart' => $ob
         ));
     }
